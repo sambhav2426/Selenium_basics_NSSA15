51feab27fc989de49c2c107e6afcf1f73e7d6327
seema <sima13570@gmail.com>
2022-04-09 22:53:18 +0530

    commit changes

---
+++ selenium/src/basics/AddedDemo.java
@@ -0,0 +1,12 @@
+package basics;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class AddedDemo {
+	@Test
+	public void m1() {
+		Reporter.log("demo File added for Git push code and check the changes there");
+	}
+	
+}
